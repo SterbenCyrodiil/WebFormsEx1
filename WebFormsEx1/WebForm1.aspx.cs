6ec51949493d4b36dd1f874ab57e9ad7f3c73412
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2021-03-01 17:28:39 +0000

    new documentation and deleted a redudant method

--- WebFormsEx1/WebForm1.aspx.cs
+++ WebFormsEx1/WebForm1.aspx.cs
@@ -8,64 +8,86 @@ using WebFormsEx1.Entities;
 
 namespace WebFormsEx1
 {
+    /// <summary>
+    /// The form to be used 
+    /// </summary>
     public partial class WebForm1 : System.Web.UI.Page
     {
+        /// <summary>
+        /// Page_S the load.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
-                refreshGridview();
+                RefreshGridview();
             }
         }
 
-
+        /// <summary>
+        /// This method is responsible for receiving, processing and inserting information from the form into the DB.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void SubmitArticle(object sender, EventArgs e)
         {
             string name = txtName.Text;
             double price = 0;
             if (!double.TryParse(txtPrice.Text, out price))
             {
-                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "Insert a valid number please", true);
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(Insert a valid number please)", true);
             }
             else
             {
                 if (name == null)
                 {
-                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "Insert a name please", true);
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(Insert a name please)", true);
                 }
                 else
                 {
                     InsertArticle(name, price);
-                    refreshGridview();
-                    clearText(); 
+                    RefreshGridview();
+                    ClearText(); 
                 }
             }
 
         }
 
+        /// <summary>
+        /// Updates the article.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void UpdateArticle(object sender, EventArgs e)
         {
             string name = txtName.Text;
             double price;
             if (!double.TryParse(txtPrice.Text, out price))
             {
-                
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(Insert a valid number please)", true);
             }
             else
             {
                 if (name == null)
                 {
-
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(Insert a name please)", true);
                 }
                 else
                 {
                     UpdatePrice(name, price);
-                    refreshGridview();
-                    clearText();
+                    RefreshGridview();
+                    ClearText();
                 }
             }
         }
 
+        /// <summary>
+        /// Searches the article.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void SearchArticle(object sender, EventArgs e)
         {
             string name = txtName.Text;
@@ -79,7 +101,7 @@ namespace WebFormsEx1
                 Article article = GetArticle(name);
                 if (article == null)
                 {
-                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Not found')", true);
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Write the name')", true);
                 }
                 else
                 {
@@ -89,25 +111,35 @@ namespace WebFormsEx1
             }
 
         }
+        /// <summary>
+        /// Deletes the article.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void DeleteArticle(object sender, EventArgs e)
         {
             string name = txtName.Text;
 
             if (name == null)
             {
-                
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Write the name')", true);
             }
             else
             {
-                deleteIntro(name);
-                refreshGridview();
-                clearText(); 
+                DeleteIntro(name);
+                RefreshGridview();
+                ClearText(); 
             }
         }
 
+        /// <summary>
+        /// When the Index changes in the Drop Drown List, places the information of said article in the boxes
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected void DDL1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Article article = getArticleListItem(DDL1.SelectedValue);
+            Article article = GetArticle(DDL1.SelectedValue);
             if (article != null)
             {
                 txtName.Text = article.Name;
@@ -115,6 +147,10 @@ namespace WebFormsEx1
             }
         }
 
+        /// <summary>
+        /// Gets the articles as list, from the DB, using Linq
+        /// </summary>
+        /// <returns>A list of Articles.</returns>
         private List<Article> GetArticlesAsList()
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
@@ -123,6 +159,10 @@ namespace WebFormsEx1
             }
         }
 
+        /// <summary>
+        /// Gets the articles as queryable, from the DB, using Linq
+        /// </summary>
+        /// <returns>An IQueryable with the Articles</returns>
         private IQueryable<Article> GetArticlesAsQueryable()
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
@@ -133,6 +173,11 @@ namespace WebFormsEx1
 
         }
 
+        /// <summary>
+        /// Gets an article searching by its unique identifier,from the DB, using Linq
+        /// </summary>
+        /// <param name="name">Name of the article</param>
+        /// <returns>The article which is being searched</returns>
         private Article GetArticle(string name)
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
@@ -142,6 +187,11 @@ namespace WebFormsEx1
 
         }
 
+        /// <summary>
+        /// Inserts a new article into the DB, using Linq.
+        /// </summary>
+        /// <param name="name">The name of the article</param>
+        /// <param name="price">The price of the article</param>
         private void InsertArticle(string name, double price)
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
@@ -160,6 +210,11 @@ namespace WebFormsEx1
             }
         }
 
+        /// <summary>
+        /// Updates the price of an article
+        /// </summary>
+        /// <param name="name">The name of the article to be updated</param>
+        /// <param name="price">The new price of the article</param>
         private void UpdatePrice(string name, double price)
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
@@ -177,7 +232,11 @@ namespace WebFormsEx1
             }
         }
 
-        private void deleteIntro(string name)
+        /// <summary>
+        /// Deletes an article from the DB, using Linq.
+        /// </summary>
+        /// <param name="name">The name of the article</param>
+        private void DeleteIntro(string name)
         {
             using (AcademiaNETEntities db = new AcademiaNETEntities())
             {
@@ -194,12 +253,10 @@ namespace WebFormsEx1
             }
         }
 
-        private Article getArticleListItem(string name)
-        {
-            return GetArticle(name);
-        }
-
-        private void refreshGridview()
+        /// <summary>
+        /// Refreshes the gridview and the Dropdown list
+        /// </summary>
+        private void RefreshGridview()
         {
             List<Article> articles = GetArticlesAsList();
             GridView1.DataSource = articles;
@@ -212,7 +269,10 @@ namespace WebFormsEx1
 
         }
 
-        private void clearText()
+        /// <summary>
+        /// Clears the text from the text boxes
+        /// </summary>
+        private void ClearText()
         {
             txtName.Text = "";
             txtPrice.Text = "";
